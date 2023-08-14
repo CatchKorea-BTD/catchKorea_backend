9f8288191b9e257e28ac4237d290fdfd8fea4455
hong <kevin0928@naver.com>
2023-08-14 21:15:38 +0900

    Fix: change entity and add db

--- src/main/java/CatchKorea/backend/entity/Post.java
+++ src/main/java/CatchKorea/backend/entity/Post.java
@@ -1,23 +1,32 @@
 package CatchKorea.backend.entity;
 
-import lombok.Builder;
+import lombok.*;
 
 import javax.persistence.*;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Post {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(nullable = false)
     private String title;
+
     @Column(nullable = false)
     private String contents;
+
     @Column(nullable = false)
     private String hashTag;
+
     @Column(nullable = false)
     private String serviceLink;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "category_id")
     private Category category;
@@ -25,13 +34,15 @@ public class Post {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "hashtag_id")
     private Hashtag hashtag;
-<<<<<<< HEAD
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "root_category_id")
     private RootCategory rootCategory;
-=======
 
-
-
->>>>>>> d8b0cfb03c5e36a669a704483939a102ba9125df
+    public Post(String title, String contents, String hashTag, String serviceLink) {
+        this.title = title;
+        this.contents = contents;
+        this.hashTag = hashTag;
+        this.serviceLink = serviceLink;
+    }
 }
