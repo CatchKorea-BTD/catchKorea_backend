a3e6ba436cc0223ef93246fadfb08e4a71430daf
hong <kevin0928@naver.com>
2023-08-17 00:44:37 +0900

    Feat: Cors 서버 주소 추가

--- src/main/java/CatchKorea/backend/config/SecurityConfig.java
+++ src/main/java/CatchKorea/backend/config/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Collections.singletonList("https://hackathon-fe-eight.vercel.app"));
+        configuration.setAllowedOrigins(Arrays.asList("https://hackathon-fe-eight.vercel.app","https://catchkorea.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(Collections.singletonList("*"));
         configuration.setAllowCredentials(true);
